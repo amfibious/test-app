751506ffd33bb58caa2934a7dc053d93df7b6bc0
Sunrise Ezekikwu <ezesunrise@yahoo.com>
2018-09-04 23:31:25 +0100

    changed template property to Object in schemas

--- models/question.js
+++ models/question.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose');
 const optionSchema = new mongoose.Schema(
     {
         value: { type: String, enum: ['A', 'B', 'C', 'D'], uppercase: true, trim: true, required: true},
-        template: { type: String, required: true },
+        template: { 
+            text: { type: String, required: true },
+            figure: [ Number ]
+        },
         isAnswer: { type: Boolean, required: true }
     }, 
     {
@@ -14,29 +17,31 @@ const optionSchema = new mongoose.Schema(
 
 const questionSchema = new mongoose.Schema(
     {
-    number: { type: Number, required: true, min: 1,
-        validate: {
-            isAsync: true,
-            validator: function(n, callback){
-                Question.findOne({ number: this.number}, (err, doc) => {
-                    callback(!doc);
-                });
-            },
-            message: 'A question with this number already exists in the database'
+        number: { type: Number, required: true, min: 1,
+            validate: {
+                isAsync: true,
+                validator: function(n, callback){
+                    Question.findOne({ number: this.number}, (err, doc) => {
+                        callback(!doc);
+                    });
+                },
+                message: 'A question with this number already exists in the database'
+            }
+        },
+        options: { 
+            type: [optionSchema],
+            validate: {
+                validator: function(ops){ 
+                    return ops.map(op => op.value).join('').match(/[ABCD]?/)
+                },
+                message: 'Options must have unique values from A to D in alphabetical order'
+            }
+        },
+        difficulty: { type: Number, required: true, default: 3, min: 1, max: 5 },
+        template: { 
+            text: { type: String, required: true },
+            figure: [ Number ]
         }
-     },
-    options: { 
-        type: [optionSchema],
-        validate: {
-            validator: function(ops){ 
-                return ops.map(op => op.value).join('').match(/[ABCD]?/)
-            },
-            message: 'Options must have unique values from A to D in alphabetical order'
-        }
-    },
-    difficulty: { type: Number, required: true, default: 3, min: 1, max: 5 },
-    template: { type: String, required: true },
-    figure: [ Number ]
     }, 
     { 
         timestamps: true 
